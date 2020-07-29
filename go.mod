b79460b67079fc951442a5e32eddd599a852c438
Jusong Chen <jusong.chen@salesforce.com>
2020-07-28 19:52:58 -0700

    add flags

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/jusongchen/aws/s3
 go 1.14
 
 require (
-	github.com/aws/aws-sdk-go v1.31.9
+	github.com/Azure/azure-amqp-common-go/v2 v2.1.0 // indirect
+	github.com/aws/aws-sdk-go v1.31.13
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.2 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/stretchr/testify v1.5.1
-	gocloud.dev v0.19.0
+	gocloud.dev v0.20.0
 )
--- main.go
+++ main.go
@@ -2,42 +2,51 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"gocloud.dev/blob"
+
+	// "gocloud.dev/blob"
+	// "gocloud.dev/blob/s3blob"
 	"gocloud.dev/blob/s3blob"
 	_ "gocloud.dev/blob/s3blob"
+	// _ "gocloud.dev/blob/s3blob"
 )
 
 func main() {
+	var bucketName, region, object string
+	flag.StringVar(&bucketName, "backet", "cpf-merlin-uswest2-1", "the bucket to operation")
+	flag.StringVar(&region, "region", "us-west-2", "the bucket region")
+	flag.StringVar(&object, "object", "cpf-foo.txt", "the target object in bucket to write to")
+
+	flag.Parse()
+	if len(flag.Args()) != 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 	var err error
-	err = blobOpenS3()
 
+	log.Printf("going to write to %s in bucket %s located in region %s", object, bucketName, region)
+	err = write2Bucket(bucketName, region, object)
 	if err != nil {
-		log.Fatalf("blobOpenS3 - Error:%v", err)
+		log.Printf("write2Bucket - Error:%v", err)
 		return
 	}
-	log.Printf("blobOpenS3 without session OK")
-
-	err = s3OpenWithSession()
-	if err != nil {
-		log.Fatalf("s3OpenWithSession - Error:%v", err)
-		return
-	}
-	log.Printf("s3OpenWithSession OK")
+	log.Printf("write2Bucket OK")
 }
 
-func s3OpenWithSession() error {
+func write2Bucket(bucketName, region, object string) error {
 
 	// Establish an AWS session.
 	// See https://docs.aws.amazon.com/sdk-for-go/api/aws/session/ for more info.
 	// The region must match the region for "my-bucket".
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2"),
+		Region: aws.String(region),
 	})
 	if err != nil {
 		return err
@@ -48,13 +57,13 @@ func s3OpenWithSession() error {
 
 	// Create a *blob.Bucket.
 	// bucket, err := s3blob.OpenBucket(ctx, sess, "s3://cpf-merlin-uswest2-1/test", nil)
-	bucket, err := s3blob.OpenBucket(ctx, sess, "cpf-merlin-uswest2-1", nil)
+	bucket, err := s3blob.OpenBucket(ctx, sess, bucketName, nil)
 	if err != nil {
 		return err
 	}
 	defer bucket.Close()
 
-	w, err := bucket.NewWriter(ctx, "foo.txt", nil)
+	w, err := bucket.NewWriter(ctx, object, nil)
 	if err != nil {
 		return err
 	}
@@ -67,44 +76,6 @@ func s3OpenWithSession() error {
 	if closeErr != nil {
 		return closeErr
 	}
-	return nil
-}
-
-func blobOpenS3() error {
-
-	// Establish an AWS session.
-	// See https://docs.aws.amazon.com/sdk-for-go/api/aws/session/ for more info.
-	// The region must match the region for "my-bucket".
-	// sess, err := session.NewSession(&aws.Config{
-	// 	Region: aws.String("us-west-1"),
-	// })
-	// if err != nil {
-	// 	return err
-	// }
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
-	defer cancel()
-
-	// Create a *blob.Bucket.
-	bucket, err := blob.OpenBucket(ctx, "s3://cpf-merlin-uswest2-1?region=us-west-2")
-	// bucket, err := s3blob.OpenBucket(ctx, sess, "s3://cpf-merlin-uswest2-1/test", nil)
-	if err != nil {
-		return err
-	}
-	defer bucket.Close()
-
-	w, err := bucket.NewWriter(ctx, "s3Spike/CentralPerfFoundations/Jusong.txt", nil)
-	if err != nil {
-		return err
-	}
-	_, writeErr := fmt.Fprintln(w, "blob.OpenBucket():Greeting from Merlin!")
-	// Always check the return value of Close when writing.
-	closeErr := w.Close()
-	if writeErr != nil {
-		return writeErr
-	}
-	if closeErr != nil {
-		return closeErr
-	}
+	log.Printf("successfully wrote to s3://%s/%s in region %s", bucketName, object, region)
 	return nil
 }
